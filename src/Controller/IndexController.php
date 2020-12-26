898ddf94ae5df4d2d617479cadd744457e36412a
SametYILMAZ <samet.yilmaz@enuygun.com>
2020-12-26 20:40:56 +0300

    Symfony skeleton project installed version 5.2.1.

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class IndexController
+ * @package App\Controller
+ */
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return new JsonResponse([]);
+    }
+}
