e96da99e588e5b1514cb76ca20fb3ce519bb4b44
SametYILMAZ <samet.yilmaz@enuygun.com>
2020-12-27 14:34:16 +0300

    Person repository test created .

---
+++ tests/PersonRepoTest.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\Tests;
+
+
+use App\Entity\Person;
+use App\Repository\PersonRepository;
+use Faker\Factory;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class PersonRepoTest
+ * @package App\Tests
+ */
+class PersonRepoTest extends TestCase
+{
+    private const EXPECTED_PERSON_COUNT = 1;
+
+    /**
+     * @test
+     */
+    public function personRepoTest(): void
+    {
+        $faker = Factory::create('tr_TR');
+
+        $person = (new Person())
+            ->setId($faker->numberBetween(1,1))
+            ->setFirstName($faker->firstName)
+            ->setLastName($faker->lastName)
+            ->setPhoneNumber($faker->phoneNumber)
+            ->setAddress($faker->address);
+
+        $personRepository = $this
+            ->createMock(PersonRepository::class);
+
+        $personRepository
+            ->method('getAllPerson')
+            ->willReturn([$person]);
+
+        self::assertCount(
+            self::EXPECTED_PERSON_COUNT,
+            $personRepository->getAllPerson()
+        );
+    }
+}
